d75086664e2c599e10c922cdfce96a6923a00e02
odymos <odysseasmoschidis@gmail.com>
2023-03-20 15:23:57 +0200

    connecting front and back end

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
 
 // Components
 import Header from "./components/Header"
@@ -8,14 +10,27 @@ import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import Home from "./components/Home"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessages from "./components/FlashMessages"
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexAppToken")))
+  const [flashMessages, setFlashMessages] = useState([])
+
+  function addFlashMessage(msg) {
+    setFlashMessages(prev => prev.concat(msg))
+  }
+
   return (
     <BrowserRouter>
-      <Header />
-
+      <FlashMessages messages={flashMessages} />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
-        <Route path="/" element={<HomeGuest />} />
+        <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+        <Route path="/post/:id" element={<ViewSinglePost />} />
+        <Route path="/create-post" element={<CreatePost addFlashMessage={addFlashMessage} />} />
         <Route path="/About-us" element={<About />} />
         <Route path="/Terms" element={<Terms />} />
       </Routes>
