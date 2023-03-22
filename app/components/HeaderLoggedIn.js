fb338a31da2cc804c869bffd9a25ad7c6d9b99fb
odymos <odysseasmoschidis@gmail.com>
2023-03-22 12:17:57 +0200

    search feature

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from "react"
 import { Link } from "react-router-dom"
 import DispatchContext from "../DispatchContext"
 import StateContext from "../StateContext"
+import { Tooltip as ReactTooltip } from "react-tooltip"
 
 function HeaderLoggedIn(props) {
   const appState = useContext(StateContext)
@@ -11,21 +12,29 @@ function HeaderLoggedIn(props) {
     appDispatch({ type: "logout" })
   }
 
+  function handleSearchIcon(e) {
+    e.preventDefault()
+    appDispatch({ type: "openSearch" })
+  }
+
   return (
     <div className="flex-row my-3 my-md-0">
-      <a href="#" className="text-white mr-2 header-search-icon">
+      <a data-tooltip-content="Search" data-tooltip-id="search" onClick={handleSearchIcon} href="#" className="text-white mr-2 header-search-icon">
         <i className="fas fa-search"></i>
       </a>
-      <span className="mr-2 header-chat-icon text-white">
+      <ReactTooltip place="bottom" id="search" className="custom-tooltip"></ReactTooltip>{" "}
+      <span data-tooltip-content="Chat" data-tooltip-id="chat" className="mr-2 header-chat-icon text-white">
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"> </span>
       </span>
-      <Link to={`/profile/${appState.user.username}`} className="mr-2">
+      <ReactTooltip place="bottom" id="chat" className="custom-tooltip"></ReactTooltip>{" "}
+      <Link data-tooltip-content="My Profile" data-tooltip-id="profile" to={`/profile/${appState.user.username}`} className="mr-2">
         <img className="small-header-avatar" src={appState.user.avatar} />
       </Link>
+      <ReactTooltip place="bottom" id="profile" className="custom-tooltip"></ReactTooltip>{" "}
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
-      </Link>
+      </Link>{" "}
       <button onClick={handleLogOut} className="btn btn-sm btn-secondary">
         Sign Out
       </button>
--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Axios from "axios"
 Axios.defaults.baseURL = "http://localhost:8080"
 import "react-tooltip/dist/react-tooltip.css"
+import { CSSTransition } from "react-transition-group"
 
 // Context
 import StateContext from "./StateContext"
@@ -23,6 +24,7 @@ import FlashMessages from "./components/FlashMessages"
 import Profile from "./components/Profile"
 import EditPost from "./components/EditPost"
 import NotFound from "./components/NotFound"
+import Search from "./components/Search"
 
 function Main() {
   const initialState = {
@@ -32,7 +34,8 @@ function Main() {
       token: localStorage.getItem("complexAppToken"),
       username: localStorage.getItem("complexAppUsername"),
       avatar: localStorage.getItem("complexAppAvatar")
-    }
+    },
+    isSearchOpen: false
   }
 
   function ourReducer(draft, action) {
@@ -46,6 +49,12 @@ function Main() {
         return
       case "flashMessage":
         draft.flashMessages.push(action.value)
+        return
+      case "openSearch":
+        draft.isSearchOpen = true
+        return
+      case "closeSearch":
+        draft.isSearchOpen = false
         return
     }
   }
@@ -80,7 +89,9 @@ function Main() {
             <Route path="/terms" element={<Terms />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-
+          <CSSTransition timeout={330} in={state.isSearchOpen} classNames="search-overlay" unmountOnExit>
+            <Search />
+          </CSSTransition>
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
