5918ae26da14c2f67910bda4c622d6a6f18263a9
odymos <odysseasmoschidis@gmail.com>
2023-03-22 18:47:26 +0200

    chat feature

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -23,9 +23,9 @@ function HeaderLoggedIn(props) {
         <i className="fas fa-search"></i>
       </a>
       <ReactTooltip place="bottom" id="search" className="custom-tooltip"></ReactTooltip>{" "}
-      <span data-tooltip-content="Chat" data-tooltip-id="chat" className="mr-2 header-chat-icon text-white">
+      <span onClick={() => appDispatch({ type: "toggleChat" })} data-tooltip-content="Chat" data-tooltip-id="chat" className={"mr-2 header-chat-icon " + (appState.unreadChatCount ? "text-danger" : "text-white")}>
         <i className="fas fa-comment"></i>
-        <span className="chat-count-badge text-white"> </span>
+        {appState.unreadChatCount ? <span className="chat-count-badge text-white">{appState.unreadChatCount < 10 ? appState.unreadChatCount : "9+"}</span> : ""}
       </span>
       <ReactTooltip place="bottom" id="chat" className="custom-tooltip"></ReactTooltip>{" "}
       <Link data-tooltip-content="My Profile" data-tooltip-id="profile" to={`/profile/${appState.user.username}`} className="mr-2">
--- app/main.js
+++ app/main.js
@@ -25,6 +25,7 @@ import Profile from "./components/Profile"
 import EditPost from "./components/EditPost"
 import NotFound from "./components/NotFound"
 import Search from "./components/Search"
+import Chat from "./components/Chat"
 
 function Main() {
   const initialState = {
@@ -35,7 +36,9 @@ function Main() {
       username: localStorage.getItem("complexAppUsername"),
       avatar: localStorage.getItem("complexAppAvatar")
     },
-    isSearchOpen: false
+    isSearchOpen: false,
+    isChatOpen: false,
+    unreadChatCount: 0
   }
 
   function ourReducer(draft, action) {
@@ -55,6 +58,18 @@ function Main() {
         return
       case "closeSearch":
         draft.isSearchOpen = false
+        return
+      case "toggleChat":
+        draft.isChatOpen = !draft.isChatOpen
+        return
+      case "closeChat":
+        draft.isChatOpen = false
+        return
+      case "incrementUnreadChatCount":
+        draft.unreadChatCount++
+        return
+      case "clearUnreadChatCount":
+        draft.unreadChatCount = 0
         return
     }
   }
@@ -92,6 +107,7 @@ function Main() {
           <CSSTransition timeout={330} in={state.isSearchOpen} classNames="search-overlay" unmountOnExit>
             <Search />
           </CSSTransition>
+          <Chat />
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
